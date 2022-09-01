078b80a18cba4e3d7ff6397c0dad54480bfe2d86
artemstarchenko82 <artemstarchenko@MacBook-Pro-Artem-2.local>
2022-09-01 12:42:01 +0300

    Подправил методы MoneyValue и обновил документацию.

--- R/MoneyValue.R
+++ R/MoneyValue.R
@@ -1,25 +1,50 @@
-#' Методы для работы с классом tinkoff.invest.schemas.MoneyValue
-
-#' MoneyValue as.double
+#' Перевод MoneyValue в double
+#' 
+#' @description Переводим класс [MoneyValue](https://tinkoff.github.io/investAPI/operations/#moneyvalue) 
+#' (денежная сумма в определенной валюте) в [base::double].
+#' 
+#' @param x Объект класса 'tinkoff.invest.schemas.MoneyValue'
 #' 
 #' @export
 as.double.tinkoff.invest.schemas.MoneyValue <- function(x){
     paste(x$units, abs(x$nano), sep = ".") |> 
-        as.numeric()
+        as.double()
+}
+
+#' MoneyValue в character
+#' 
+#' @description Переводим класс [MoneyValue](https://tinkoff.github.io/investAPI/operations/#moneyvalue) 
+#' (денежная сумма в определенной валюте) в [base::character].
+#' 
+#' @param x Объект класса 'tinkoff.invest.schemas.MoneyValue'
+#' 
+#' @return Результат в формате `"123.6 rub"`, `"233.8 usd"` и т.п.
+#' 
+#' 
+#' @export
+
+as.character.tinkoff.invest.schemas.MoneyValue <- function(x){
+    paste(as.double(x), x$currency)
 }
 
 
-#' Print MoneyValue
+
+
+
+#' Печать MoneyValue
+#' 
+#' @description Выводим класс [MoneyValue](https://tinkoff.github.io/investAPI/operations/#moneyvalue) 
+#' на экран. 
+#' 
+#' @param x Объект класса 'tinkoff.invest.schemas.MoneyValue'
+#' 
+#' @return Выводим на экран применение к x метода [as.character.tinkoff.invest.schemas.MoneyValue()]
+#' 
+#' 
 #' 
 #' @export
+
 print.tinkoff.invest.schemas.MoneyValue <- function(x){
-    print(paste(as.double(x), x$currency))
+    print(as.character(x))
 }
 
-
-#' Format MoneyValue
-#' 
-#' @export
-format.tinkoff.invest.schemas.MoneyValue <- function(x){
-    format(paste(as.double(x), x$currency))
-}
