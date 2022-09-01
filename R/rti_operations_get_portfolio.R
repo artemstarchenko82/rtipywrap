3f5a8fc8099634b7449fcb0cc8d5f100f79f0c72
artemstarchenko82 <artemstarchenko@MacBook-Pro-Artem-2.local>
2022-09-01 17:30:23 +0300

    Обновил создание data.frame() из PortfolioResponse

--- R/rti_operations_get_portfolio.R
+++ R/rti_operations_get_portfolio.R
@@ -23,13 +23,92 @@ rti_operations_get_portfolio <- function(
 
 #' Перевод объекта PortfolioResponse в data.frame
 #' 
+#' @description Конструируем data.frame из данных объекта 
+#' [PortfolioResponse](https://tinkoff.github.io/investAPI/operations/#portfolioresponse)
+#' 
+#' @param x объект класса 'tinkoff.invest.schemas.PortfolioResponse'
+#' 
 #' @export
 as.data.frame.tinkoff.invest.schemas.PortfolioResponse <- function(x){
     pd <- reticulate::import("pandas")
     
     # Переводим список позиций в data.frame
-    df <- x$positions |> 
-        pd$DataFrame()  
+    position <- x$positions
+    
+    df <- data.frame(
+        figi = 
+            position |> 
+            vapply(\(x) x$figi, character(1)),
+        
+        instrument_type = 
+            position |> 
+            vapply(\(x) x$instrument_type, character(1)),
+        
+        quantity = 
+            position |> 
+            lapply(\(x) x$quantity) |> 
+            vapply(as.double, double(1)),
+        
+        average_position_price = 
+            position |> 
+            lapply(\(x) x$average_position_price) |> 
+            vapply(as.double, double(1)), 
+        
+        average_position_price_currency = 
+            position |> 
+            lapply(\(x) x$average_position_price$currency) |> 
+            vapply(as.character, character(1)), 
+        
+        expected_yield = 
+            position |> 
+            lapply(\(x) x$expected_yield) |> 
+            vapply(as.double, double(1)),
+        
+        current_nkd = 
+            position |> 
+            lapply(\(x) x$current_nkd) |> 
+            vapply(as.double, double(1)), 
+        
+        current_nkd_currency = 
+            position |> 
+            lapply(\(x) x$current_nkd$currency) |> 
+            vapply(as.character, character(1)), 
+        
+        average_position_price_pt = 
+            position |> 
+            lapply(\(x) x$average_position_price_pt) |> 
+            vapply(as.double, double(1)), 
+        
+        current_price = 
+            position |> 
+            lapply(\(x) x$current_price) |> 
+            vapply(as.double, double(1)),
+        
+        current_price_currency = 
+            position |> 
+            lapply(\(x) x$current_price$currency) |> 
+            vapply(as.character, character(1)), 
+        
+        average_position_price_fifo = 
+            position |> 
+            lapply(\(x) x$average_position_price_fifo) |> 
+            vapply(as.double, double(1)),       
+        
+        average_position_price_fifo_currency =
+            position |> 
+            lapply(\(x) x$average_position_price_fifo$currency) |> 
+            vapply(as.character, character(1)), 
+        
+        quantity_lots =
+            position |> 
+            lapply(\(x) x$quantity_lots) |> 
+            vapply(as.double, double(1)),
+        
+        blocked = 
+            position |> 
+            vapply(\(x) x$blocked, logical(1))
+    )
+    
     
     # Добавляем аттрибуты из других полей объекта 
     attr(df, "expected_yield") <- as.double(x$expected_yield)
